9a6e2df23789b1e0953c1b79288bdb7471aa744e
samuel <samuel@localhost>
2012-08-28 18:57:29 -0300

    Refiz a base do projeto para corrigr o bug da classe não aparecendo, testa agora

---
+++ CargaGeral/src/java/DAO/teste.java
@@ -0,0 +1,14 @@
+package DAO;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author samuel
+ */
+public class teste {
+    
+}
